7c8893b784adedf2251899eb994d56944961032d
swapon789 <kumarshopon850@gmail.com>
2022-10-27 11:57:43 +0600

    courser category and id added

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express")
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>{
-    res.send("News Api Running");
+app.use(cors());
+
+// const categories = require('./data/Category.json');
+const  course = require('./data/CourseContent.json')
+
+app.get('/', (req, res) => {
+    res.send("learning Api Running");
 });
 
-app.listen(port, () =>{
+app.get('/course-categories', (req, res) => {
+    res.send(course);
+});
+
+
+app.get('/course-categories/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c.id === id);
+    res.send(selectedCourse)
+});
+
+
+app.listen(port, () => {
     console.log("learnin It server Running on port", port);
-})
+});
